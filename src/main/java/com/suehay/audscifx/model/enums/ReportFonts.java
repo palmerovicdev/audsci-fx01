fafad3664322910e767cdc436e46a47554400c56
palmerodev <victor.palmero@undostres.com.mx>
2023-12-31 18:50:11 -0500

    implement some methods in ReportService

---
+++ src/main/java/com/suehay/audscifx/model/enums/ReportFonts.java
@@ -0,0 +1,27 @@
+package com.suehay.audscifx.model.enums;
+
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Font;
+
+public enum ReportFonts {
+    CAT_FONT(new Font(
+            com.itextpdf.text.Font.FontFamily.TIMES_ROMAN, 18,
+            com.itextpdf.text.Font.BOLD)),
+    RED_FONT(new Font(
+            com.itextpdf.text.Font.FontFamily.TIMES_ROMAN, 12,
+            com.itextpdf.text.Font.NORMAL, BaseColor.RED)),
+    SUB_FONT(new Font(
+            com.itextpdf.text.Font.FontFamily.TIMES_ROMAN, 16,
+            com.itextpdf.text.Font.BOLD)),
+    SMALL_BOLD(new Font(
+            com.itextpdf.text.Font.FontFamily.TIMES_ROMAN, 12,
+            com.itextpdf.text.Font.BOLD));
+    private final Font font;
+    ReportFonts(Font font) {
+        this.font = font;
+    }
+    public Font getFont() {
+        return font;
+    }
+}
